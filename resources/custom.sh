8db952a665004a3c80952f9e7b91eecf90c45cea
LoRd_MuldeR <mulder2@gmx.de>
2026-06-14 16:13:05 +0200

    Added the required dependencies + force update of installed packages.

--- resources/custom.sh
+++ resources/custom.sh
@@ -3,3 +3,10 @@
 set -exu
 
 # Add your additional provisioning here for custom VM images.
+pkg_add -u
+pkg_add git
+pkg_add -Dsnap ngtcp2 nghttp3 curl rust rust-src
+
+mkdir -p /opt/sysroot/i386 /opt/sysroot/arm64
+curl -sSf https://cdn.openbsd.org/pub/OpenBSD/7.9/i386/base79.tgz  | tar -C /opt/sysroot/i386  -xzf - ./usr/lib
+curl -sSf https://cdn.openbsd.org/pub/OpenBSD/7.9/arm64/base79.tgz | tar -C /opt/sysroot/arm64 -xzf - ./usr/lib
